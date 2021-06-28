139ae199f389bb25097a82ac011b3aecde4290b8
davidOC007 <75745959+davidOC007@users.noreply.github.com>
2021-06-28 16:40:40 +0200

    Correctif regexSauce

--- backend/middleware/regexSauce.js
+++ backend/middleware/regexSauce.js
@@ -1,9 +1,32 @@
+const fs = require('fs');
+
+const inputSauce = {
+    name: "^[^\\s][a-zA-Zéèàêûçàôë\\s-]{2,40}$",
+    manufacturer: "^[^\\s][a-zA-Zéèàêûçàôë'\\s-]{2,25}$",
+    description: "^[^\\s][a-zA-Zéèàêûçàôëî.':,!\\s-]{10,1000}$",
+    mainPepper: "^[^\\s][a-zA-Zéèàêûçàôë',\\s-]{2,45}$",
+    };
+
 module.exports = (req,res,next) => {
-    const inputSauce = /^[a-zA-Zéèàêûçàôëî.':,!\\s-]{2,1000}$/
-    const pattern = new RegExp(inputSauce);
-    if(pattern.test(req.body.password)) {
-        next();
+    let error400 = false;
+    if (req.body.sauce) {
+        sauceObject = JSON.parse(req.body.sauce);
     } else {
-        res.status(400).json({ message: "Veuillez vérifier le contenu de vos champs !"});
+        sauceObject = {...req.body};
+    }
+    for (let key in inputSauce) {
+        const pattern = new RegExp(inputSauce[key]);
+        if (pattern.test(sauceObject[key]) == false) {
+            error400 = true;
+        }
+    }
+    if (error400 == true || req.body.errorMessage) {
+        res.status(400).json({ message: "error" });
+        fs.unlink(`images/${req.file.filename}`, () => {
+            console.log("La requête ne correspond pas aux informations attendues")
+        });
+    }
+    else {
+        next();
     }
 }
